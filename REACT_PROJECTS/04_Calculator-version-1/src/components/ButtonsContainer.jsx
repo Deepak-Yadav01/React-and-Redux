07f4a86791661704ed7a1a860ef5793091785c6e
Deepak Yadav <dspyadavdeepak01@gmail.com>
2026-01-10 05:25:55 +0530

    increase readabilit

--- REACT_PROJECTS/04_Calculator-version-1/src/components/ButtonsContainer.jsx
+++ REACT_PROJECTS/04_Calculator-version-1/src/components/ButtonsContainer.jsx
@@ -1,22 +1,35 @@
 import styles from "./ButtonsContainer.module.css";
 
 const ButtonsContainer = () => {
-
-    const buttonNames = ['c', '1', '2', '+', '3', '4', '-', '5', '6', '*', '7', '8', '/', '=', '9', '0', '.'];
-
-
+  const buttonNames = [
+    "c",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
 
   return (
     <div>
       <div className={styles.buttonsContainer}>
-
-        {buttonNames.map(buttonName =><button className={styles.button}>
-            {buttonName}
-        </button> )}
-          
-        </div>
+        {buttonNames.map((buttonName) => (
+          <button className={styles.button}>{buttonName}</button>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ButtonsContainer;
